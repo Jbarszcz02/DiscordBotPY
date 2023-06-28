3e8d5bc971754396a6e4c0b5d1909b2b4175cf24
Kuba <j.barszcz02@gmail.com>
2023-06-28 15:43:23 +0200

    Rest of the file

--- app.py
+++ app.py
@@ -72,7 +72,45 @@ async def leave(ctx):
         await ctx.send("The bot is not connected to a voice channel.")
         
         
-            
+@bot.command(name='play_song', help='To play song')
+async def play(ctx, url):
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+        
+@bot.command(name='pause', help='Pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("Bot is not playing anything at the moment.")
+        
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send('The bot was not playing anything')
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        voice_client.stop()
+    else:
+        await ctx.send('The bot is not playing anything right now')
     
+if __name__ == "__main__" :
+    bot.run(DISCORD_TOKEN)
+        
 
 
